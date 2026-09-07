35eea96d23be162da84d5301553fd2fb8b06998e
Volodymyr <danvolodym@gmail.com>
2026-09-07 11:13:09 +0300

    Add files via upload

---
+++ пара1/script.js
@@ -0,0 +1,81 @@
+//42; - Number
+//4.5; - Number
+
+//"use Str";
+
+//true;
+//1
+
+//false;
+//0;
+//NaN;
+//null;
+//undefined;
+
+//x = y + 10
+
+//var a;
+//a = 10;
+
+//console.log(a);
+
+//var b = 20;
+//var c = 12, d = 12;
+
+//let aNew = "32";
+
+// a = 13;
+// let a;
+// console.log(a);
+
+// const a = 4;
+
+// let a = 15, b = 26;
+// console.log(a + b);
+// console.log(b - b);
+// console.log(b * a);
+// console.log(b ** a);
+// console.log(b / a);
+// console.log(b % a);
+//
+// a += 1;
+// a++;
+// a--;
+// ++a;
+
+// let a = 2, b;
+// //b = a++;
+// b = ++a;
+// console.log(b);
+
+// let a = "4";
+// let b = 2;
+//
+// console.log(a - b);
+// console.log(a + b);
+// console.log((a - 0) + b);
+//
+// a = Number(a);
+// console.log(a + b);
+
+// інтерпеляція - формат рядкового значення (f строка)
+
+// let none = "Vovan";
+// let age = 99;
+//
+// console.log("Привіт" + none + "!" + "тобі" + age + "років");
+// console.log(`Привіт ${none}! тобі ${age} рокыв`);
+
+// console.info("hello world!");
+// console.log("hello world!");
+// console.warn("hello world!");
+// console.error("hello world!");
+
+// alert("hello");
+// let name = prompt("Як тебе звати?");
+// alert(name);
+
+let a = Number(prompt("number1?"));
+let b = +prompt("number2?");
+console.log(a + b);
+
